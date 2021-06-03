92dfd4984820af12b2e188e65a5e1d1a9b25536e
Blake Bourque <Techplex.Engineer@gmail.com>
2021-06-02 22:53:44 -0400

    Working tree view

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,19 +3,72 @@
 <script>
 	import Modal from 'svelte-simple-modal';
 	import HelloWorld from './components/HelloWorld.svelte'
-	import logo from './logo.png';
 
 	(async () =>{
 		console.log(await window.backend.basic())
 	})()
+
+	let tabs = [
+		{ name: "Tree", active: true },
+		{ name: "Tab 1", active: false },
+		{ name: "Tab 2", active: false },
+	];
+	let active = "Tree";
+
+
+	import Tree from "./components/tree.svelte";
+    $: tree = [];
+
+	(async (t) => {
+	    let keys = await window.backend.Client.GetKeys("");
+		keys = keys.sort()
+        let level = {tree};
+
+        keys.forEach(path => {
+            path.split('/').reduce((r, name, i, a) => {
+                if(!r[name]) {
+                    r[name] = {tree: []};
+                    const obj = {name, children: r[name].tree};
+                    if (name === "") { obj.name="Root" }
+                    r.tree.push(obj)
+                }
+
+                return r[name];
+            }, level)
+        })
+		tree = tree; // make the reactivity work
+		console.log(JSON.stringify(tree, null, 4))
+
+	})(tabs);
+	tabs.push({ name: "Tab 2", active: false })
+
 </script>
 
 <main>
 
-	<div class="container">
+	<ul class="nav nav-tabs mt-1">
+		{#each tabs as tab}
+		<li class="nav-item">
+			<a class="nav-link"
+			   class:active={tab.name == active}
+			   href="#"
+			   on:click={() => active = tab.name}
+			>
+				{tab.name}
+			</a>
+		</li>
+		{/each}
+	</ul>
+
+
+	<div class="container mt-2">
 		<div class="row">
-			<div class="col">col1</div>
-			<div class="col">col2</div>
+			<div class="col">
+                <Tree {tree} let:node>
+                    <div class="name">{node.name}</div>
+                </Tree>
+			</div>
+			<div class="col"></div>
 		</div>
 	</div>
 
